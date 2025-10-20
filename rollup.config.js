42b65a96a02f1467b841f3c342da74c526db0dbe
Michael Vattuone <mvattuone@gmail.com>
2025-10-20 13:36:40 -0400

    chore: Remove unnecessary json plugin for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 // rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 
 export default {
   input: 'index.js',
@@ -19,7 +18,6 @@ export default {
     commonjs({
       requireReturnsDefault: 'auto', 
     }),
-    json({ preferConst: true })
   ]
 };
 
